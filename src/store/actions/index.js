81cd9d05b5f8265571ffe816aafe27c2eced29cc
greatefekodo <greatefekodo@gmail.com>
2025-10-14 05:16:57 +0100

    updated

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,7 +3,8 @@ import api from "../../api/api"
 export const fetchProducts = (queryString) => async (dispatch) => {
     try {
         dispatch({ type: "IS_FETCHING"});
-        const { data } = await api.get(`/public/products?${queryString}`);
+        const { data } = await api.get(`/public/products${queryString ? '?' + queryString : ''}`);
+
         dispatch({
             type: "FETCH_PRODUCTS",
             payload: data.content,
